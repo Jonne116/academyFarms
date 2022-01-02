584f7f656a7ceebae6879b0db1c70601bc0ccfd2
Jonne Lehtonen <jonne_0@hotmail.com>
2022-01-02 15:30:58 +0200

    Added backend tests again

--- src/test/java/dev/academy/academyFarms/service/FarmServiceTest.java
+++ src/test/java/dev/academy/academyFarms/service/FarmServiceTest.java
@@ -1,22 +1,32 @@
 package dev.academy.academyFarms.service;
 
+import dev.academy.academyFarms.model.Farm;
 import dev.academy.academyFarms.repository.FarmRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.time.Instant;
+import java.util.*;
+
 public class FarmServiceTest {
 
-    private FarmRepository farmRepository = Mockito.mock(FarmRepository.class);
+    private final FarmRepository farmRepository = Mockito.mock(FarmRepository.class);
+    FarmService farmService = new FarmService(farmRepository);
 
     @Test
-    @DisplayName("Should get farms when passing parameters")
-    void getFarms() {
-        FarmService farmService = new FarmService(farmRepository);
-        Mockito.when(farmRepository.findAllByParams())
-    }
-
-    @Test
+    @DisplayName("Should get location and sensorType lists")
     void getLocations() {
+
+        Mockito.when(farmRepository.getLocations()).thenReturn(new ArrayList<>(Collections.singleton("New York")));
+        Mockito.when(farmRepository.getSensorTypes()).thenReturn(new ArrayList<>(Collections.singleton("humidity")));
+
+        Farm farm = new Farm("New York", Instant.now(), "humidity", 25.11F);
+        Map<String, List<String>> result = new HashMap<>();
+        result.put("location", farmRepository.getLocations());
+        result.put("sensorType", farmRepository.getSensorTypes());
+
+        Assertions.assertThat(farmService.getLocationsSensors()).isEqualTo(result);
     }
 }
